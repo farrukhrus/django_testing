70f9c7c926776feefabf68982798454d2c7e713d
Farrukh <frustamov@openwaygroup.com>
2023-10-01 15:51:52 +0200

    yanews + *.py files

--- ya_note/notes/tests/test_content.py
+++ ya_note/notes/tests/test_content.py
@@ -28,22 +28,6 @@ class Tests(TestCase):
         cls.url_add = reverse('notes:add')
         cls.url_edit = reverse('notes:edit', args=(cls.note.slug,))
 
-    def test_is_note_in_context(self):
-        """
-        Test if note is in context's object_list.
-        Must be True for note's author and False otherwise.
-        """
-        test_cases = (
-            (self.author_client, True),
-            (self.guest_client, False),
-        )
-        for users_client, expected_outcome in test_cases:
-            with self.subTest(users_client):
-                response = users_client.get(self.list_url)
-                object_list = response.context['object_list']
-                statement = self.note in object_list
-                self.assertEqual(statement, expected_outcome)
-
     def test_notes_list(self):
         """
         в список заметок одного пользователя
